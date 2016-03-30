a6e2a93c4a56b343a5f16de25a36cc3be053edf7
Sonali Marne <Sonali Marne>
2016-03-29 20:48:20 -0500

    First Commit

---
+++ Client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 8080                 # Reserve a port for your service.
+print('Connecting to port: ',port)
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
